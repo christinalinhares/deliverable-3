5c5e431a963a2e2e42ba57c7da2fd6a8b3455074
Christina Linhares <6314995@edu.vaniercollege.qc.ca>
2025-11-07 13:06:38 -0500

    identifying and managing missing values, correction data types and formats
    Filling in the blank values with either the mean of the column for numeric columns or "UNKNOWN" for non-numeric columns, no need to correct data types and formats

--- deliverable3.py
+++ deliverable3.py
@@ -13,6 +13,7 @@ top_5_audience_score = data["Audience score %"].value_counts().head(5)
 print(top_5_audience_score)
 #showing top 5 values of worldwide gross and audience score within dataset
 
+#b)
 print(data.shape)
 #show how many rows and columns
 
@@ -27,3 +28,15 @@ print(data.duplicated().sum())
 data = data.drop_duplicates()
 #find how many duplicates there are and delete them
 
+#c
+data["Genre"] = data["Genre"].fillna("Unknown")
+data["Lead Studio"] = data["Lead Studio"].fillna("Unknown")
+data["Audience score %"] = data["Audience score %"].fillna(data["Audience score %"].mean())
+data["Profitability"] = data["Profitability"].fillna(data["Profitability"].mean())
+data["Rotten Tomatoes %"] = data["Rotten Tomatoes %"].fillna(data["Rotten Tomatoes %"].mean())
+data["Worldwide Gross"] = data["Worldwide Gross"].fillna(data["Worldwide Gross"].mean())
+data["Year"] = data["Year"].fillna(data["Year"].mean())
+#since the data set is small rather than removing rows with missing values their values were inputted using the mean for numerical columns to maintain a reasonable size for the data set. If the column did not have numerical values then "Unknown" was inputted into the cell.
+
+#d
+#This step is unecessary as no data type needs to be corrected
